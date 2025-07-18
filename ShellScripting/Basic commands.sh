1e31c842bf470cd244521911f37509d1944bd06e
Abhigyan Khaund <160063176+DevAbhi99@users.noreply.github.com>
2025-07-18 22:17:56 +0100

    pushed shell scripting basic command

---
+++ ShellScripting/Basic commands.sh
@@ -0,0 +1,39 @@
+#Shell scripting
+
+
+#Directory Management commands
+
+pwd #shows current directory
+
+ls -l #shows list of directories in long listed format
+
+mkdir #make directory
+
+cp file1.txt file2.txt  #moves from one file to another
+
+mv file.txt dirname #moves file from one directory to another or folder from one directory to another
+
+rm file.txt #removes file
+
+touch file.txt #creates an empty file
+
+nano file.txt #creates editable file
+
+
+#Text processing management
+
+cat file.txt # displays the content of the file
+
+grep 'word' file.txt  #searches for pattern in the file
+
+sort file.txt  #sort the content of the file
+
+head file.txt #displays the first few lines of the file
+
+tail file.txt #displays the last few lines of the file
+
+wc file.txt #displays the count of lines in the file
+
+rmdir #removes folder
+
+
